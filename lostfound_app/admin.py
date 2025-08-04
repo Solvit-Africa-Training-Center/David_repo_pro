d12fe72d811786ca97eff54ed3972520b0ad5e92
Nkejumuremyi <nkejumuremyidavid@gmail.com>
2025-08-04 15:25:54 +0200

    customised admin portal

--- lostfound_app/admin.py
+++ lostfound_app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Item
 
+admin.site.site_header = "Lost & Found admin portal"
+admin.site.site_title = "admin portal"
+
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('title', 'post_type', 'user', 'location', 'date', 'created_at')
